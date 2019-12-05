e6d366460004b9dc759bb98c41b8bf5d385c5ea0
alan <alan_macuco@yahoo.com.br>
2019-12-05 12:48:05 -0300

    ajustando as chamadas

--- APIGateway/index2.js
+++ APIGateway/index2.js
@@ -1,12 +1,9 @@
 //index.js
-require('dotenv-safe').config();
+require("dotenv-safe").config();
 var jwt = require('jsonwebtoken');
-
 var http = require('http');
 const express = require('express')
 const httpProxy = require('express-http-proxy')
-var bodyParser = require('body-parser');
-
 const app = express()
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -14,6 +11,14 @@ const helmet = require('helmet');
 
 const userServiceProxy = httpProxy('http://localhost:3001');
 const productsServiceProxy = httpProxy('http://localhost:3002');
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(logger('dev'));
+app.use(helmet());
+app.use(cookieParser());
 
 // Proxy request
 // Proxy request
@@ -24,13 +29,11 @@ app.get('/users', verifyJWT, (req, res, next) => {
 app.get('/products', verifyJWT, (req, res, next) => {
     productsServiceProxy(req, res, next);
 })
-
 //authentication
-app.post('/entra', (req, res) => {
-  //console.log(req.body);  
-    console.log(res.body);
+app.post('/login', (req, res, next) => {
 
-    if (req.body.user === 'luiz' && req.body.pwd === '123') {
+    console.log(req.body)
+    if (req.body.user === 'usu' && req.body.pwd === '123') {
         //auth ok
         const id = 1; //esse id viria do banco de dados
         var token = jwt.sign({
@@ -46,15 +49,7 @@ app.post('/entra', (req, res) => {
 
     res.status(500).send('Login inválido!');
 })
-//logout
-app.get('/logout', function (req, res) {
-    res.status(200).send({
-        auth: false,
-        token: null
-    });
-});
 
-//autorizacao
 function verifyJWT(req, res, next) {
     var token = req.headers['x-access-token'];
     if (!token) return res.status(401).send({
@@ -73,13 +68,14 @@ function verifyJWT(req, res, next) {
         next();
     });
 }
-app.use(logger('dev'));
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
+
+app.get('/logout', function (req, res) {
+    res.status(200).send({
+        auth: false,
+        token: null
+    });
+});
+
 
 var server = http.createServer(app);
 server.listen(3000);
